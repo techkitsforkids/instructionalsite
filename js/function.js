25f9d881d06548a97fa13e29bc24871025747fce
techkitsforkids <violeta.torres@perotmuseum.org>
2021-01-01 23:04:59 -0600

    fix issue with local storage

--- js/function.js
+++ js/function.js
@@ -1,13 +1,24 @@
 
-if(localStorage.getItem('textValues') == null){
+if(localStorage.getItem('textValuest1') == null){
 	var table1Names =[];
-	var table2Names =[];
-	var table3Names =[];
 }else{
-	table1Names =  JSON.parse(localStorage.getItem('textValues'));
-	table2Names =  JSON.parse(localStorage.getItem('textValues'));
-	table3Names =  JSON.parse(localStorage.getItem('textValues'));
+	table1Names =  JSON.parse(localStorage.getItem('textValuest1'));
 }
+
+if(localStorage.getItem("textValuest2") == null){
+		var table2Names =[];
+}
+else {
+		table2Names =  JSON.parse(localStorage.getItem('textValuest2'));
+}
+
+if(localStorage.getItem("textValuest3") == null){
+		var table3Names =[];
+}
+else {
+		table3Names =  JSON.parse(localStorage.getItem('textValuest3'));
+}
+
 
 function testJS() {
     var b = document.getElementById('name').value,
@@ -52,7 +63,7 @@ function saveObjectT1(){
 	  table1Names.push(guest); 
 	  
 	  table1Names.sort(function(a, b){return b.grams-a.grams});
-	  	localStorage.setItem('textValues', JSON.stringify(table1Names));
+	  	localStorage.setItem('textValuest1', JSON.stringify(table1Names));
 	  x=1;
 		for(i=0; i <table1Names.length; ++i){
 		
@@ -111,7 +122,7 @@ function saveObjectT2(){
 	  table2Names.push(guest); 
 	  
 	  table2Names.sort(function(a, b){return b.grams-a.grams});
-	  localStorage.setItem('textValues', JSON.stringify(table2Names));
+	  localStorage.setItem('textValuest2', JSON.stringify(table2Names));
 	  x=1;
 		for(i=0; i <table2Names.length; ++i){
 		
@@ -170,7 +181,7 @@ function saveObjectT3(){
 	  table3Names.push(guest); 
 	  
 	  table3Names.sort(function(a, b){return b.grams-a.grams});
-	  localStorage.setItem('textValues', JSON.stringify(table3Names));
+	  localStorage.setItem('textValuest3', JSON.stringify(table3Names));
 	  x=1;
 		for(i=0; i <table3Names.length; ++i){
 		
@@ -195,49 +206,49 @@ function saveObjectT3(){
 }
 
 function table1Results(){
-			var parsed = "table 1\n";
-		 
-		for (i = 0; i< table1Names.length; i++) {
-			var myobj=  table1Names[i];
-			
-			for (var property in myobj) {
-				parsed += property + ": " + myobj[property] + "\n";          
+			var parsedT1 = "table 1\n";
+		 			console.log(table1Names);
+		for (it1 = 0; it1< table1Names.length; it1++) {
+			var myobjt1 =  table1Names[it1];
+
+			for (var propertyt1 in myobjt1) {
+				parsedT1 += propertyt1 + ": " + myobjt1[propertyt1] + "\n";          
 			}
 		} 
-		
-		$("#display").val(parsed); 
+
+		$("#display").val(parsedT1); 
 
 	return false;
 }
 
 function table2Results(){
-			var parsed = "table 2\n";
+			var parsedt2 = "table 2\n";
 		
-		for (i = 0; i< table2Names.length; i++) {
-			var myobj=  table2Names[i];
+		for (it2 = 0; it2< table2Names.length; it2++) {
+			var myobjt2 =  table2Names[it2];
 			
-			for (var property in myobj) {
-				parsed += property + ": " + myobj[property] + "\n";          
+			for (var propertyt2 in myobjt2) {
+				parsedt2 += propertyt2 + ": " + myobjt2[propertyt2] + "\n";          
 			}
 		} 
 		
-		$("#display2").val(parsed); 
+		$("#display2").val(parsedt2); 
 
 	return false;
 }
 
 function table3Results(){
-			var parsed = "table 3\n";
+			var parsedt3 = "table 3\n";
 		 
-		for (i = 0; i< table3Names.length; i++) {
-			var myobj=  table3Names[i];
+		for (it3 = 0; it3< table3Names.length; it3++) {
+			var myobjt3 =  table3Names[it3];
 			
-			for (var property in myobj) {
-				parsed += property + ": " + myobj[property] + "\n";          
+			for (var propertyt3 in myobjt3) {
+				parsedt3 += propertyt3 + ": " + myobjt3[propertyt3] + "\n";          
 			}
 		} 
 		
-		$("#display3").val(parsed); 
+		$("#display3").val(parsedt3); 
 
 	return false;
 }
